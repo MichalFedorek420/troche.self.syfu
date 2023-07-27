caf0adc3d3529dbea2ba9c16d7ddf227cb23e72c
MichalFedorek420 <michal.fedorek2@gmail.com>
2023-07-27 16:02:13 +0200

    L_F_G

---
+++ PRIVATESTUFF2/cities.py
@@ -0,0 +1,21 @@
+import csv
+import mysql.connector
+
+with mysql.connector.connect(user='sql7635727', password='qc4yj4GflZ', host='sql7.freemysqlhosting.net', database='sql7635727') as connection:
+    cursor = connection.cursor()
+
+
+    with open ('cities.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            try:
+                previous_position = int(row['2021'])
+            except:
+                previous_position = 0
+            sql = f"""INSERT INTO
+            City(name, country, position, previous_position)
+            VALUES('{row['City']}', '{row['Country']}', {int(row['2022'])}, {previous_position})
+            """
+            cursor.execute(sql)
+        connection.commit()
